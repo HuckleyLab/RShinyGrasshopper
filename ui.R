1245d8b2e39099289655ee02dbaeca4b40d4a2ba
Your full name <your_github_email_@email.com>
2019-05-10 12:10:17 -0700

    Adds popup ability

--- ui.R
+++ ui.R
@@ -13,6 +13,7 @@
 
 library(shiny)
 library(tidyverse)
+library(shinyalert)
 
 #specify choices
 specs= c("Aeropedellus clavatus", "Camnula pellucida", "Melanoplus dawsoni", "Melanoplus sanguinipes", "Melanoplus boulderensis")
@@ -84,4 +85,9 @@ fluidPage(
 
 
 )
+)
+
+ui <- fluidPage(
+  useShinyalert(),  # Set up shinyalert
+  actionButton("preview", "Preview")
 )
